80115efbe5a8940ee5ad97880896648d647a789e
abdullah14120 <abdullah.tamemii@gmail.com>
2026-04-21 22:13:51 +0300

    Update StartActivity.java

--- app/src/main/java/com/ban/ab/StartActivity.java
+++ app/src/main/java/com/ban/ab/StartActivity.java
@@ -25,7 +25,7 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.layout_start);
 
         btnStart = findViewById(R.id.btnStartNext);
         
